e8b28bc90d55f0b0d8ab3ad8faac790fd1ae3bf6
omar khattab <omar.khattab7@icloud.com>
2023-12-26 17:50:48 +0200

    Initial commit

---
+++ DynamicArray/DynamicArray.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    class DynamicArray
+    {
+        public int[] arr;
+        int LastIndexItem;
+        public DynamicArray(int size)
+        {
+            arr = new int[size];
+            LastIndexItem = -1;
+        }
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= arr.Length)
+                {
+                    throw new Exception("error!!");
+                }
+                else
+                    return arr[index];
+            }
+            set
+            {
+                if (index < 0 || index >= arr.Length)
+                    throw new Exception("error!!");
+                else
+                    arr[index] = value;
+            }
+        }
+        public void ExpandStorage()
+        {
+            int[] arr2 = new int[arr.Length * 2];
+            Array.Copy(arr, arr2, arr.Length);
+            arr = arr2;
+        }
+        public void Add(int value)
+        {
+            if (LastIndexItem == arr.Length - 1)
+            {
+                ExpandStorage();
+            }
+            LastIndexItem++;
+            arr[LastIndexItem] = value;
+        }
+        public void Print()
+        {
+            for (int i = 0; i <= LastIndexItem; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        public int find(int key)
+        {
+            for (int i = 0; i <= LastIndexItem; i++)
+            {
+                if (arr[i] == key)
+                    return i;
+            }
+            return -1;
+        }
+        public int Length()
+        {
+            return LastIndexItem + 1;
+        }
+        public int StorageLength()
+        {
+            return arr.Length;
+        }
+    }
+}
---
+++ DynamicArray/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using array;
+class program
+{
+    public static void Main()
+    {
+        DynamicArray DA = new DynamicArray(3);
+        DA.Add(13);
+        DA.Add(39);
+        DA.Add(75);
+        Console.WriteLine($"number of elments which assigned : {DA.Length()}"); Console.WriteLine($"Array length : {DA.StorageLength()}");//Before Expanding
+        DA.Add(67);
+        Console.WriteLine($"number of elments which assigned : {DA.Length()}"); Console.WriteLine($"Array length : {DA.StorageLength()}");//After Expanding
+        DA.Print();
+    }
+}
